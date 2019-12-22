2d3c5ad5c47318a575a86e48b19661500621dad7
kennethvdborne <44980914+kennethvdborne@users.noreply.github.com>
2019-12-22 13:46:30 +0100

    Adjustments to classes

--- EEVA.Domain/Models/Teacher.cs
+++ EEVA.Domain/Models/Teacher.cs
@@ -5,7 +5,9 @@ using System.Text;
 namespace EEVA.Domain.Models
 {
     public class Teacher : Contact
-
     {
+        public Teacher(string firstName, string lastName, string email, string phone) : base(firstName, lastName, email, phone)
+        {
+        }
     }
 }
