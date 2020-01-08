79101ea0f2544202f4d43da28d113fc19d8627b5
Matthias Somay <Matthias Somay@DESKTOP-DQNERB6>
2020-01-08 13:32:56 +0100

    chartjs test

--- EEVA/Controllers/HomeController.cs
+++ EEVA/Controllers/HomeController.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using EEVA.Models;
+using EEVA.Domain.DataManager;
+using EEVA.Domain;
+using EEVA.Web.Models;
+using static EEVA.Web.Models.Chart;
 
 namespace EEVA.Controllers
 {
@@ -13,19 +17,40 @@ namespace EEVA.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ContactManager _contactManager;
+
+
+
+        public HomeController(EEVAContext context, ILogger<HomeController> logger)
         {
+            _contactManager = new ContactManager(context);
             _logger = logger;
         }
 
         public IActionResult Index()
         {
 
-
-
             return View();
 
         }
+        public JsonResult BarChartData()
+        {
+            Chart _chart = new Chart();
+            _chart.labels = new string[] { "Jan", "Feb", "Mar" };
+            _chart.datasets = new List<Datasets>();
+            List<Datasets> _dataSet = new List<Datasets>();
+            _dataSet.Add(new Datasets()
+            {
+                label = "This Year",
+                data = new int[] { 40, 60, 20 },
+                backgroundColor = new string[] { "800000", "#E9967C", "#FF0000" },
+                borderColor = new string[] { "800000", "#E9967C", "#FF0000" },
+                borderWidth = "1"
+            });
+            _chart.datasets = _dataSet;
+            return Json(_chart);
+        }
+
 
         public IActionResult Privacy()
         {
