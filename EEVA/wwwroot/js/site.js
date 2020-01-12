e36c8844966449751dcb6716a56a3688d78e90be
kennethvdborne <44980914+kennethvdborne@users.noreply.github.com>
2020-01-12 12:48:58 +0100

    minor adjustment

--- EEVA/wwwroot/js/site.js
+++ EEVA/wwwroot/js/site.js
@@ -1,13 +1,7 @@
 ﻿$(document).ready(function () {
-    $("#success-alert").fadeTo(2000, 500).slideUp(500, function () {
-        $("#success-alert").slideUp(500);
+    $(".alert").fadeTo(2000, 500).slideUp(500, function () {
+        $(".alert").slideUp(500);
 
-    });
-});
-
-$(function () {
-    $('div.form-group .chkTrt').on('click', function () {
-        $('div.form-group .chkTrt').not(this).prop('checked', false);
     });
 });
 
